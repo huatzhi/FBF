107356afe8c09ab75ac16c7a4c90e92a343dcece
Huat Zhi <huatzhi@gmail.com>
2021-09-16 12:09:06 +0800

    feat: add grouped csv output

---
+++ scripts/4.1/options/options.js
@@ -0,0 +1,10 @@
+const options = {
+  instrumentsLimit: [],
+  timeFrameLimit: [],
+  indicatorKeyLimit: [],
+  indicatorTypeLimit: [],
+  resultKey: "Basic(14,1_2,3_3,10)",
+  includeDateTime: true,
+};
+
+module.exports = options;
